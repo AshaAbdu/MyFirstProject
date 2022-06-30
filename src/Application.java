233d8a47024b72dd28b51421a775965bee893c3a
AshaAbdu <halge87@yahoo.com>
2022-06-29 20:19:31 -0500

    second commit

--- src/Application.java
+++ src/Application.java
@@ -2,8 +2,38 @@
 public class Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//This is a multi comment
+		/* Two main types of data
+		 * Primitive
+		 * they are:
+		 * int -an integer
+		 * double - decimal places 32.89235
+		 * float - precision decimal data type 45.3987
+		 * long - like an int numbers can be higher - differentiate with an L
+		 * short - like an int, less of number range
+		 * byte - 8 bits of data
+		 * char - a single character - a, A, B & with single quotes
+		 * boolean - can only be two values i.e True or False
+		 * 
+		 * Objects
+		 * String - textual data, a string of characters like a word or sentence 
+		 */
+		
+		 //this is a single comment 
+		//variable declaration - type, identifier, assignment operator, value, semicolon
+		int age = 30;
+		double accountBalance = 55.67;
+		char middleInitial = 'H';
+		boolean isHotOutside = true;
+		String firstName = "Asha";
+		
+		//shortcut sysout +control+space
+		System.out.println(age);
+		System.out.println(accountBalance);
+		System.out.println(middleInitial);
+		System.out.println(isHotOutside);
+		System.out.println(firstName);
+		
 	}
 
 }
