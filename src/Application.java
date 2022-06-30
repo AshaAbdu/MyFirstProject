0f1ee1da38332de138b14506234cbe0f1b43cd7c
AshaAbdu <halge87@yahoo.com>
2022-06-29 21:27:37 -0500

    third commit - Operators

--- src/Application.java
+++ src/Application.java
@@ -20,20 +20,41 @@ public class Application {
 		 */
 		
 		 //this is a single comment 
-		//variable declaration - type, identifier, assignment operator, value, semicolon
-		int age = 30;
+		//variable declaration - type, identifier, assignment operator, value, semicolon/ meaningful
+		//variables use camel case naming convention -every name after the first name is capitalized  
+		int ashasAge = 30;
 		double accountBalance = 55.67;
 		char middleInitial = 'H';
 		boolean isHotOutside = true;
 		String firstName = "Asha";
 		
 		//shortcut sysout +control+space
-		System.out.println(age);
+		System.out.println(ashasAge);
 		System.out.println(accountBalance);
 		System.out.println(middleInitial);
 		System.out.println(isHotOutside);
 		System.out.println(firstName);
 		
+		//operators performs actions on operands
+		// + - * / 
+		
+		int age = 32;
+		System.out.println(age);
+		
+		String fistName2 = "Ibrahim"; //a space after a string can make a space in output 
+		String lastName2 = "Mohamed";
+		String fullName = fistName2 + " " +lastName2; //concatenation also empty quotes creates space
+		System.out.println(fullName);
+		
+		//equality operator uses double equals to see if something is equal
+		boolean isAge30 = age == 30;
+		System.out.println(isAge30);
+		
+		//without assigning it to variable
+		System.out.println(age == 32);
+		
+		//comparing identical expressions
+		System.out.println(30 == 57);
 	}
 
 }
